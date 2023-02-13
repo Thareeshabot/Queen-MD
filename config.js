d3b0fea7c7a3210b774123e9da251f45d75386d6
Thareeshabot <117811989+Thareeshabot@users.noreply.github.com>
2023-02-14 00:28:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
 global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
 global.author = process.env.STICKER_AUTHOR_NAME === undefined ? '👿චුට්ටා👿' : process.env.STICKER_AUTHOR_NAME
-global.pmblock = process.env.PM_BLOCK === undefined ? 'true' : process.env.PM_BLOCK
+global.pmblock = process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK
 global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'true' : process.env.PM_CHATBOT
 global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'false' : process.env.GROUP_CHATBOT
 
